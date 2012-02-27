b9309e6b2372e6122ba56061f274758097e683f4
Apirak Panatkool <apirakb@gmail.com>
2012-02-27 11:18:38 +0700

    create first partial

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,15 @@
 module MessagesHelper
+
+  def show_tag(message)
+    tag_s = ""
+    unless message.tags.blank? 
+      if message.tags.length > 0 
+        tag_s = "tags: "
+      else
+        tag_s = "tag: "
+      end
+      tag_s = tag_s + message.tags.join(", ")
+    end
+    return tag_s
+  end
 end
