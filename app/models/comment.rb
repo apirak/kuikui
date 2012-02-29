59342dec60e106d9d7849baefda0af8dc9a4c09e
Apirak Panatkool <apirakb@gmail.com>
2012-02-29 21:48:13 +0700

    add unit test for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :message
+
+  validates :message_id, :presence => true
 end
--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid comment" do
+    comment = Comment.new
+    assert comment.invalid?
+    assert comment.errors[:message_id].any?
+  end
+
+  test "valid comment" do
+    message = messages(:one)
+    message.save
+    comment = message.comments.new
+    assert comment.valid?
+    assert comment.save
+  end
 end
